9a1ac3282e495afecc5e6298ff335a1436f23a9c
matt4446 <barmy_matt@hotmail.com>
2012-03-18 23:47:28 +0000

    test

---
+++ AdminMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace JsonProjection
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public AdminMenu()
+        {
+        }
+
+        public Localizer T { get; set; }
+        public string MenuName { get { return "admin"; } }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.AddImageSet("navigation")
+                .Add(T("Json Query Feeds"), "9",
+                    menu =>
+                    {
+                        menu.Add(T("Preview"), "0", item => item.Action("Index", "Service", new { area = "JsonProjection" }));
+                    });
+        }
+    }
+}
---
+++ Routes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace JsonProjection
+{
+    public class Routes : IRouteProvider
+    {
+        public string Area 
+        {
+            get { return "JsonProjection"; } 
+        }
+        
+        public Routes()
+        {
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                             new RouteDescriptor {
+                                                     Route = new Route(
+                                                         "Admin/JsonProjection/Preview/{id}",
+                                                         new RouteValueDictionary {
+                                                                                      {"area", this.Area},
+                                                                                      {"controller", "Service"},
+                                                                                      {"action", "Preview"}
+                                                                                  },
+                                                         new RouteValueDictionary(),
+                                                         new RouteValueDictionary {
+                                                                                      {"area", this.Area }
+                                                                                  },
+                                                         new MvcRouteHandler())
+                                                 }
+                         };
+        }
+    }
+}
